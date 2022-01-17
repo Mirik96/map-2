11b54be911e88e3c38b9ca3907b58bb3be3a7371
mirik <123@mail.ru>
2022-01-17 23:14:12 +0500

    all

---
+++ script.js
@@ -0,0 +1,10 @@
+let users = [
+    { name: "Вася", surname: "Пупкин", id: 1 },
+    { name: "Петя", surname: "Иванов", id: 2 },
+    { name: "Маша", surname: "Петрова", id: 3 }
+];
+
+
+let user = users.map((el) => el.name + el.surname + el.id)
+alert(user)
+
